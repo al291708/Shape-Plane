144992af3916cf95045dd9f399596a2f6b3b5538
JonAndoniFernandez <al315182@uji.es>
2017-09-27 23:58:16 +0800

    Proyecto Con movimiento
    Proyecto inicial Unity con movimiento y cámara

---
+++ Shape Plane Project/Assets/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform objetaASeguir;
+	public float suavidadMovimiento;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetPosition = new Vector3 (objetaASeguir.position.x, objetaASeguir.position.y + 3.15f, objetaASeguir.position.z - 5);
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * suavidadMovimiento);
+	}
+}
