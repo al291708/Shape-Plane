8b71bf18b9a9b7ada1fe282c4069dca5ec8de137
dharach <al315185@uji.es>
2017-10-18 13:21:26 +0200

    Game Over Añadido

---
+++ Shape Plane Project/Assets/Scripts/Menus/GameOverManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour {
+
+	public ToyPlane planeLifes;
+	public float restarDelay = 5f;
+
+	Animator anim;
+	float restartTime;
+
+	// Use this for initialization
+	void Awake () {
+		
+		anim = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (planeLifes.isAlive() == false) {
+		
+			anim.SetTrigger ("GameOver");
+
+
+		}
+		
+	}
+}
